420ba88d1384cba51637dc41c6d3ff9b7138b409
Vitalii Bondar <vitalik.bondarqq@gmail.com>
2020-04-18 20:34:50 +0300

    refactor and create first component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from "components/common";
+
+function App() {
+  return (
+    <div className="App">
+     <h1>App</h1>
+        <Button>Test</Button>
+    </div>
+  );
+}
+
+export default App;
